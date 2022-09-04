0500568c3cd2d3fd2695b17c75e938e559a326d7
Sudip Das <sudip5428@gmail.com>
2022-09-05 00:33:27 +0530

    role and permission fix

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-
+use Spatie\Permission\Models\Permission;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 
@@ -25,7 +25,7 @@ class RoleController extends Controller
         $validated = $request->validate(['name' => ['required', 'min:3']]);
         Role::create($validated);
 
-        return to_route('admin.roles.index')->with('message', 'Role Created successfully.');
+        return redirect()->route('admin.roles.index')->with('message', 'Role Created successfully.');
     }
 
     public function edit(Role $role)
@@ -39,7 +39,7 @@ class RoleController extends Controller
         $validated = $request->validate(['name' => ['required', 'min:3']]);
         $role->update($validated);
 
-        return to_route('admin.roles.index')->with('message', 'Role Updated successfully.');
+        return redirect()->route('admin.roles.index')->with('message', 'Role Updated successfully.');
     }
 
     public function destroy(Role $role)
