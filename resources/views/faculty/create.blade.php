7250c43f120cc0c1329a1ff57d0970db59dde693
Sudip Das <sudip5428@gmail.com>
2022-09-06 21:02:19 +0530

    Docker and docker compose added

--- resources/views/faculty/create.blade.php
+++ resources/views/faculty/create.blade.php
@@ -1,1 +1,9 @@
-<h1>faculty </h1>
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+   
+</x-admin-layout>    
