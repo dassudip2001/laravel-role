66216ebae27a00a9f6be23a9a349d4525f2febf5
Sudip Das <sudip5428@gmail.com>
2022-09-15 22:13:26 +0530

    design cheanges

--- resources/views/faculty/create.blade.php
+++ resources/views/faculty/create.blade.php
@@ -1,157 +1,155 @@
 <x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-{{--            {{ __('Dashboard') }}--}}
+         {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    @role('admin')
-    <div class="container mx-auto">
-        <div class="grid grid-cols-2 gap-2">
-
-            <div>
-                <div class="min-h-screen py-12 bg-gray-100 flex ">
-                    <div class="container max-w-screen-lg mx-auto">
-
-
-                        <form action=" " method="POST">
-                            @csrf
-                            <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-                                <div class="grid gap-1 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-                                    <div class="text-gray-600">
-                                        <p class="font-medium text-lg">Faculty</p>
-                                        <p>Please fill out all the fields.</p>
-                                    </div>
-
-                                    <div class="lg:col-span-2">
-                                        <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                                            <div class="md:col-span-3">
-                                                <label for="fac_title"> Faculty Title </label>
-                                                <input type="text" name="fac_title" id="fac_title" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" />
-                                            </div>
-
-
-
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_code">Faculty Code</label>
-                                                <input type="text" name="fac_code" id="fac_code" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_designtion">Faculty Designtion</label>
-                                                <input type="text" name="fac_designtion" id="fac_designtion" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_join">Faculty Join</label>
-                                                <input type="text" name="fac_join" id="fac_join" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_retirement">Faculty Retirement</label>
-                                                <input type="text" name="fac_retirement" id="fac_retirement" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_phone">Faculty Phone</label>
-                                                <input type="number" name="fac_phone" id="fac_phone" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_status">Faculty Status</label>
-                                                <input type="text" name="fac_status" id="fac_status" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-                                            <div class="md:col-span-6">
-                                                <label for="fac_description">Faculty Description</label>
-                                                <input type="text" name="fac_description" id="fac_description" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
-                                            </div>
-
-                                            <div class="md:col-span-6 text-right">
-                                                <div class="inline-flex items-end">
-                                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
+   <div class="container mt-4">
+    <div class="row">
+        <div class="col">
+            <!-- Select Form -->
+            <!-- card -->
+            <div class="card">
+                <!-- card title -->
+                <div class="card-title mx-3 mt-2">
+                    <h6>Faculty</h6>
+                    <hr>
+                </div>
+                <form action="" method="POST">
+                    @csrf
+                <div class="card-body">
+                    <!-- card body -->
+                    <div class="row">
+                        <div class="col">
+                            <!-- Faculty Title -->
+                            <div class="mb-6">
+                            <label for="faculty_title">Faculty Title<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_title"  id="faculty_title" aria-describedby="faculty_title" placeholder="Enter  Faculty Title">   
                             </div>
-
-
-                        </form>
+                        </div>
+                        <div class="col">
+                            <!-- Faculty Code -->
+                            <div class="mb-6">
+                            <label for="faculty_code">Faculty Code<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_code"  id="faculty_code" aria-describedby="faculty_code" placeholder="Enter  Faculty Code">   
+                           </div>
+                        </div>
                     </div>
-                </div>3</div>
-            <div>
+                    <div class="row">
+                        <div class="card">
+                            <!-- Faculty Designation -->
+                            <div class="mb-6">
+                            <label for="faculty_designation">Faculty Designation<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_designtion"  id="faculty_designation" aria-describedby="faculty_designation" placeholder="Enter  Faculty Designation">   
+                           </div>
+                        </div>
+                        <div class="card">
+                            <!-- Faculty Join -->
+                            <div class="mb-6">
+                            <label for="faculty_join">Faculty Join<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_join"  id="faculty_join" aria-describedby="faculty_join" placeholder="Enter  Faculty Join">   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <!-- Faculty Retirement -->
+                            <div class="mb-6">
+                            <label for="faculty_retirement">Faculty Retirement<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_retirement"  id="faculty_retirement" aria-describedby="faculty_retirement" placeholder="Enter  Faculty Retirement">   
+                           </div>
+                        </div>
+                        <div class="col">
+                            <!-- Faculty Phone -->
+                            <div class="mb-6">
+                            <label for="faculty_phone">Faculty Phone<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_phone"  id="faculty_phone" aria-describedby="faculty_phone" placeholder="Enter  Faculty Phone Number">   
+                          </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <!-- Faculty Status -->
+                            <div class="mb-6">
+                            <label for="faculty_status">Faculty Status<span class="required" style="color: red;">*</span></label>
+                            <input type="text" class="form-control form-control-sm" name="fac_status"  id="faculty_status" aria-describedby="faculty_status" placeholder="Enter  Faculty Status">   
+                            </div>
+                        </div>
+                        <div class="col">
+                            <!-- Faculty Description -->
+                            <div class="mb-6">
+                             <label for="faculty_description">Faculty Description<span class="required" style="color: red;">*</span></label>
+                             <input type="text" class="form-control form-control-sm" name="fac_description"  id="faculty_description" aria-describedby="faculty_description" placeholder="Faculty Description">   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Button -->
+                <div class="mx-2 mt-2">
+                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>            
 
-{{--second--}}
-@endrole
-  <div class="min-h-screen py-12 bg-gray-100 flex ">
-                    <div class="container max-w-screen-lg mx-auto">
-                            <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-                                <div class="grid gap-1 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-                                <table class="min-w-max w-full">
-                                    <thead>
-                                    <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                                        <th class="py-3 px-6 text-left">ID</th>
-                                        <th class="py-3 px-6 text-left">Title</th>
+                </div>
+                </form>
+            </div>
+        </div>
+        <div class="col">
+            <!-- Show Form -->
+            <div class="card">
+                <!-- card title -->
+            <div class="card-title mx-3 mt-2">
+                <h6>Faculty Form</h6>
+                <hr>
+            </div>
+            <div class="card-body">
+                <!-- table Show data -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Code</th>
+                            <th>Designation</th>
+                            <th>Join</th>
+                            <th>Retirement</th>
+                            <th>Phone</th>
+                            <th>Status</th>
+                            <th>Description</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($faculty as $item)
+                        <tr>
+                            <td>{{$item->id}}</td>
+                            <td> {{$item->fac_title}}</td>
+                            <td>{{$item->fac_code}}</td>
+                            <td>{{$item->fac_designtion}}</td>
+                            <td>{{$item->fac_join}}</td>
+                            <td>{{$item->fac_retirement}}</td>
+                            <td>{{$item->fac_phone}}</td>
+                            <td>{{$item->fac_status}}</td>
+                            <td>{{$item->fac_description}}</td>
+                            <th> 
+                             @role('admin')
+                             <a href=" {{ url('/faculty/edit',$item->id) }} ">
+                              <i class="fa-regular fa-pen-to-square"></i>
+                             </a>
+                            <a href=" {{ url('/faculty/delete',$item->id) }} ">
+                            <button type="submit"><i class="fa-solid fa-trash"></i></button>
+                            @endrole
+                             </th>
+                        </tr>
+                        @endforeach
+                    </tbody>
 
-                                        <th class="py-3 px-6 text-center">Code</th>
-                                        <th class="py-3 px-6 text-center">Designtion</th>
-                                        <th class="py-3 px-6 text-center">Join</th>
-                                        <th class="py-3 px-6 text-center">Retirement</th>
-                                        <th class="py-3 px-6 text-center">Phone</th>
-                                         <th class="py-3 px-6 text-center">Status</th>
-                                        <th class="py-3 px-6 text-center">Describption</th>
-
-
-                                        <th class="py-3 px-6 text-center">Actions</th>
-                                    </tr>
-                                    </thead>
-
-                                    <tbody>
-                                       @foreach ($faculty as $item)
-                                    <tr>
-                                    <td>{{$item->id}}</td>
-                                    <td> {{$item->fac_title}}</td>
-                                    <td> {{$item->fac_code}}</td>
-
-                                     <td> {{$item->fac_designtion}}</td>
-                                      <td> {{$item->fac_join}}</td>
-                                       <td> {{$item->fac_retirement}}</td>
-                                        <td> {{$item->fac_phone}}</td>
-                                        <td> {{$item->fac_status}}</td>
-                                        <td> {{$item->fac_description}}</td>
-                                    <td>
-
-                                        <th class="py-3 px-6 text-left">
-                                            <a href="  {{ url('/faculty/edit',$item->id) }}">
-                                                <div class="inline-flex items-end">
-                                                 <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                                               </div>
-                                            </a>
-                                        </th>
-                                        <th class="py-3 px-6 text-left">
-                                            <a href=" {{ url('/faculty/delete',$item->id) }}">
-                                                <div class="inline-flex items-end">
-                                                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                                                </div>
-                                            </a>
-                                        </th>
-                                    </td>
-                                </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-
+                </table>
+            </div>
+            </div>
+            
         </div>
     </div>
-        </div>
-    </div>
-    </div>
-    <!-- component -->
+
+   </div>
 
 
 </x-admin-layout>
